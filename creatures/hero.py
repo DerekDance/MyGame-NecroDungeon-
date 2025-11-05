68c4755c8086553a0be9f657804614f19385ffe7
DerekDance <thamuz.ru@gmail.com>
2025-11-05 16:17:27 +0300

    creating a Hero class and using it in the modified main.py code

---
+++ creatures/hero.py
@@ -0,0 +1,16 @@
+"""
+Класс Героя
+"""
+class Hero:
+    def __init__(self):
+        self.hero_health = 20
+        self.hero_max_health = 20
+        self.hero_attack = 3
+        self.hero_range_attack = 6
+        self.hero_gold = 1
+        self.hero_potion_strength = 0
+        self.hero_potion_heal = 0
+        self.count_crit_attack  = 0
+        self.hero_scroll_of_sparks = 0
+        self.bullet_of_sparks = 0
+        self.damage_bullet_of_sparks = 12
