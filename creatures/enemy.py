3991caf83a6388b72e1f45c210f647cf3bee470a
DerekDance <thamuz.ru@gmail.com>
2025-11-07 12:40:08 +0300

    обновил файл enemy и main.повысил сложность боя с учеником-некромантом, добавил класс NecroStudent

--- creatures/enemy.py
+++ creatures/enemy.py
@@ -8,50 +8,56 @@ class Enemy:
         self.max_health = health
         self.attack = attack
         self.distance = distance
-
-
-    def take_damage(self, damage: int):
-        self.health = max(0, self.health - damage)
-        return self.health
+        self.charge_turns = 0
 
 
     def is_alive(self) -> bool:
         return self.health > 0
 
-"""
-Классы противников
-"""
-
-class Dummy(Enemy):
-    def __init__(self):
-        super().__init__("Тренировочный манекен",20,1,3)
-
-
-class Acolyte(Enemy):
-    def __init__(self):
-        super().__init__("Аколит",9,2,5)
-        self.charge_turns = 0
-
-    def update(self):
+    """
+    Для подготовки заряженных заклинаний:
+    Комната 1 - Заряжание арбалета Аколита,
+    Комната 2 - Заклинание Ученика-некроманта "Могильный хват"
+    """
+    def update(self,sum_steps,step):
         """Вызывается каждый ход — обновляет состояние Аколита"""
-        if self.charge_turns < 3:
-            self.charge_turns += 1
-        # Когда достигает 3 — остаётся 3, пока не выстрелит
+        if self.charge_turns < sum_steps:
+            self.charge_turns += step
 
-    def can_shoot(self):
+    def can_active(self,ready_step):
         """Можно ли стрелять?"""
-        return self.charge_turns == 3
+        return self.charge_turns == ready_step
 
     def reset_charge(self):
         """Сбросить заряд после выстрела"""
         self.charge_turns = 0
 
+# Классы противников
 
+# Манекен
+class Dummy(Enemy):
+    def __init__(self):
+        super().__init__("Тренировочный манекен",20,1,3)
+
+
+# Аколит
+class Acolyte(Enemy):
+    def __init__(self):
+        super().__init__("Аколит",9,2,5)
+        self.charge_turns = 0
+
+
+
+# Ученик-некроманта
 class NecroStudent(Enemy):
     def __init__(self):
         super().__init__("Ученик-некроманта",9,2,3)
+        self.charge_turns = 0
 
 
+
+
+# Субстанция
 class MainSubstance(Enemy):
     def __init__(self):
         super().__init__("Субстанция",30,3,6)
@@ -67,7 +73,7 @@ class SubMini2(Enemy):
         super().__init__(
             "\u001b[35mСклизкая субстанция\u001b[0m",11,2,1)
 
-
+# Некромант
 class Necromancer(Enemy):
     def __init__(self):
         super().__init__("\u001b[35mНекромант\u001b[0m",30,3,3)
